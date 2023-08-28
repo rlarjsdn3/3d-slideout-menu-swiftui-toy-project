b9d2fd098c20272a9224600309edfb13f9977bfb
Zizi_Kim <rlarjsdn3@naver.com>
2023-08-28 22:44:11 +0900

    초기 프로젝트 파일 추가

---
+++ ScrollViewWithDynamicSections/ScrollViewWithDynamicSections/ScrollViewWithDynamicSectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewWithDynamicSectionsApp.swift
+//  ScrollViewWithDynamicSections
+//
+//  Created by 김건우 on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewWithDynamicSectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
